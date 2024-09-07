eb46ab3b9a3f8c9d72434ea1e1b8b32714dde6e3
Chris Cabang <ccscabang@gmail.com>
2024-09-07 12:08:55 +0200

    cleanup: removed unecessary salt
    Removed salting as part of the API. Callers can just perform
    update with salt.

--- tests/vectors.h
+++ tests/vectors.h
@@ -20,15 +20,9 @@ enum SHAlength {
 
 struct vector {
   const char *input;
-  const char *salt;
   const char *expected;
 };
 
-// A fox jumps over the lazy dog + 5P0n9b0b5qu@r3P@nt5
-// b45e1a776384d53368dd4869f32466c38e605e6995aad25a6d221973bb07bf2a17f055bf0d87471d9e069d2ef01e3e51aacb8f1a135c384a92e7d4ef0520e8ab
-// abc + 5P0n9b0b5qu@r3P@nt5
-// 2e16c237eb058414bc6859c6e2e2143c63db406f20349f3daed749feac699a217913f1d41a0cd8df1fe557ea8c8b48c878ff6124ddaf907fe72ad1e1fee67049
-// Test arrays for hashes
 struct Hash {
   enum SHAfamily name;
   int size;
@@ -42,35 +36,31 @@ struct Hash {
     { 
       {
         "abc",
-        0,
         "ddaf35a193617abacc417349ae204131"
         "12e6fa4e89a97ea20a9eeee64b55d39a"
         "2192992a274fc1a836ba3c23a3feebbd"
         "454d4423643ce80e2a9ac94fa54ca49f"
       },
       {
-        "abc",
-        "5P0n9b0b5qu@r3P@nt5",
-        "2e16c237eb058414bc6859c6e2e2143c"
-        "63db406f20349f3daed749feac699a21"
-        "7913f1d41a0cd8df1fe557ea8c8b48c8"
-        "78ff6124ddaf907fe72ad1e1fee67049"
+        "5P0n9b0b5qu@r3P@nt5abc",
+        "24339ecda3d58ce27c7f073293ca13a4"
+        "b51e99b6edd2afcb94b551af0d670539"
+        "83bda56a0afdc0a9a0f9386514593488"
+        "d2a2be6ae7bef99338da16ab234adcba"
       },
       {
         "",
-        0,
         "cf83e1357eefb8bdf1542850d66d8007"
         "d620e4050b5715dc83f4a921d36ce9ce"
         "47d0d13c5d85f2b0ff8318d2877eec2f"
         "63b931bd47417a81a538327af927da3e"
       },
       {
-        "A fox jumps over the lazy dog",
-        "5P0n9b0b5qu@r3P@nt5", 
-        "b45e1a776384d53368dd4869f32466c3"
-        "8e605e6995aad25a6d221973bb07bf2a"
-        "17f055bf0d87471d9e069d2ef01e3e51"
-        "aacb8f1a135c384a92e7d4ef0520e8ab"
+        "5P0n9b0b5qu@r3P@nt5A fox jumps over the lazy dog", 
+        "3185346b571dad5d3b3a3a2d7daaa344"
+        "dcef217372d9c8cb2304e523e9f3b257"
+        "35a80a8f00da7b4e434d34f48304e23b"
+        "b0981d67893c16d171f2eeb76bc694e7"
       }
     }
   }
